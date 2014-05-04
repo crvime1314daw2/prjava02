09d2f4ac6973bd98dbcb9af416fee5922ed608e9
crvime1314daw2 <crvime1314daw2@gmail.com>
2014-05-04 17:16:28 +0200

    Treballant amb ordres Git i branques v0.2

--- src/prjava02/Prjava02.java
+++ src/prjava02/Prjava02.java
@@ -20,6 +20,7 @@ public class Prjava02
             System.out.println("Sistema operatiu: " + System.getProperty("os.name")); 
             System.out.println("Versió OS: " + System.getProperty("os.version"));
 	    System.out.println("Arquitecura OS: " + System.getProperty("os.arch"));
+            System.out.println("Afegint més codi a la branca00 del projecte prjava02");
         }
         catch (IOException e) 
         { 
